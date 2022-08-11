37007eae98316c382395eda7af89ad992df6e2fa
Tobi-Akindele <oyindamolaakindele3@icloud.com>
2022-08-11 18:29:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ehr-server-app
---
+++ src/test/java/com/tobiakindele/ehr/server/ehrserverapp/EhrServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobiakindele.ehr.server.ehrserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
